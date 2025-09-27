6ad79bd857c941d501a68ce9d14727701734d411
Edmart <edmart666@gmail.com>
2025-09-27 10:30:16 +0300

    Сделал задачу showSalary

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
   // ваш код...
+  let result = "";
+  for (i = 0; i < users.length; i++) {
+    console.log(users[i].age <= age);
+    if (users[i].age <= age) {
+      let userDate = users[i].name + ", " + users[i].balance + "\n";
+      console.log(userDate);
+      result += userDate;
+    }
+  }
+  return result.slice(0, result.length - 1);
 }
