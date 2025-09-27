c8f384c5db18a0778167b97d1926b3dd4257915d
Edmart <edmart666@gmail.com>
2025-09-27 10:35:20 +0300

    Удалил логироание из функции showSalary

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -2,10 +2,9 @@ function showSalary(users, age) {
   // ваш код...
   let result = "";
   for (i = 0; i < users.length; i++) {
-    console.log(users[i].age <= age);
     if (users[i].age <= age) {
       let userDate = users[i].name + ", " + users[i].balance + "\n";
-      console.log(userDate);
+
       result += userDate;
     }
   }
