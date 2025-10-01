fd7d1cbc152ff47139270d6f0f5ff2ac1167ff9d
Edmart <edmart666@gmail.com>
2025-10-01 09:36:11 +0300

    highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,32 @@
+const ageIndex = 1;
+const genderIndex = 2;
+const statusIndex = 3;
+
+
 function highlight(table) {
   // ваш код...
+  let bodyTable = table.tBodies[0];
+  for (row of bodyTable.rows) {
+    // вложенные циклы это плохо, но я не знаю как еще сделать эту задачу более элегантно
+    // html колеекции не имеют методов ни map, ни foreach
+    for (let cell of row.cells) {
+      if (cell.cellIndex === ageIndex && cell.textContent < 18) {
+        row.style = "text-decoration: line-through";
+      }
+
+      if (cell.cellIndex === genderIndex && cell.textContent === "m") {
+        row.classList.add("male");
+      } else if (cell.cellIndex === genderIndex && cell.textContent === "f") {
+        row.classList.add("female");
+      }
+
+      if (cell.cellIndex === statusIndex && cell.dataset.available === "true") {
+        row.classList.add("available");
+      } else if (cell.cellIndex === statusIndex && cell.dataset.available === "false") {
+        row.classList.add("unavailable");
+      } else if (cell.cellIndex === statusIndex && cell.dataset.available === undefined) {
+        row.setAttribute("hidden", "true");
+      }
+    }
+  }
 }
