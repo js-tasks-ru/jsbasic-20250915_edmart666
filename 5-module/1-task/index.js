ecf7c8a64946d62d3c5180a8ea7c8371c74d1735
Edmart <edmart666@gmail.com>
2025-10-05 18:03:49 +0300

    Сделал задачу hideSelf

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,8 @@
 function hideSelf() {
   // ваш код...
+  let buttonHideSelf = document.querySelector('.hide-self-button');
+
+  buttonHideSelf.addEventListener('click', (event) => {
+    event.target.hidden = true;
+  })
 }
